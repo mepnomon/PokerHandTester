35211ac99fb75ab9f5e67075a94e9146e6615f38
Dorian Dressler <fourgigatonmantis@gmail.com>
2018-10-06 20:51:30 +0100

    final commit before clean-up

--- src/main/java/com/poker/PokerHand.java
+++ src/main/java/com/poker/PokerHand.java
@@ -1,5 +1,7 @@
 package com.poker;
 
+import java.util.HashMap;
+import java.util.Map;
 import static java.lang.String.valueOf;
 
 public class PokerHand {
@@ -10,20 +12,24 @@ public class PokerHand {
 		TIE;
 	}
 
-
-	private RankFactory rankFactory;
 	private String hand;
+	private RankFactory rankFactory;
 
-	public PokerHand(String hand, RankFactory rankFactory) {
+	public PokerHand(String hand) {
 
-		this.rankFactory = rankFactory;
+		this.rankFactory = new RankFactory();
 		this.hand = hand;
 	}
 
 	public Result compareWith(PokerHand opponentsHand) {
 
-		Rank opponentsRank = opponentsHand.getRank();
+		if(getRank() == Rank.TWO_PAIRS && opponentsHand.getRank() == Rank.TWO_PAIRS) {
+			return getHighestTwoPairs(opponentsHand);
+		}
 
+		if(getRank() == Rank.THREE_OF_A_KIND && opponentsHand.getRank() == Rank.THREE_OF_A_KIND){
+			return getHigherThreeOfAKind(opponentsHand);
+		}
 		if(getRank().value() < opponentsHand.getRank().value()){
 			return Result.LOSS;
 		}
@@ -31,10 +37,86 @@ public class PokerHand {
 		if(getRank().value() > opponentsHand.getRank().value()){
 			return Result.WIN;
 		}
-		return compareHighCard(opponentsHand, opponentsRank);
+
+
+		return compareHighCard(opponentsHand);
 	}
 
-	private Result compareHighCard(PokerHand opponentsHand, Rank opponentsRank) {
+	private Result getHighestTwoPairs(PokerHand opponentsHand){
+		final int OCCURRENCE = 2;
+		String[] opponentsCards = cardStringToArray(opponentsHand.getHand());
+		String[] myCards = cardStringToArray(getHand());
+		HashMap<String, Integer> cardMap = getStringIntegerHashMap(myCards);
+		HashMap<String, Integer> oppopentCardMap = getStringIntegerHashMap(opponentsCards);
+		Card[] myHighestCard = extractHighestCardsFromMap(cardMap);
+		Card[] opponentHighestCard = extractHighestCardsFromMap(oppopentCardMap);
+		int mySum = 0;
+		int opponentSum = 0;
+		for(int i = 0; i < myHighestCard.length; i++){
+			mySum += myHighestCard[i].value();
+			opponentSum += opponentHighestCard[i].value();
+		}
+
+		if (mySum > opponentSum) {
+			return Result.WIN;
+		}
+
+		if (mySum < opponentSum) {
+			return Result.LOSS;
+		}
+
+		return Result.TIE;
+	}
+	private Result getHigherThreeOfAKind(PokerHand opponentsHand){
+
+		final int OCCURRENCE = 3;
+		String[] opponentsCards = cardStringToArray(opponentsHand.getHand());
+		String[] myCards = cardStringToArray(getHand());
+		HashMap<String, Integer> cardMap = getStringIntegerHashMap(myCards);
+		HashMap<String, Integer> oppopentCardMap = getStringIntegerHashMap(opponentsCards);
+		Card myHighestCard = extractHighestCardFromMap(cardMap,OCCURRENCE);
+		Card opponentHighestCard = extractHighestCardFromMap(oppopentCardMap, OCCURRENCE);
+
+		if(myHighestCard.value() < opponentHighestCard.value()){
+			return Result.LOSS;
+		}
+
+		if(myHighestCard.value() > opponentHighestCard.value()){
+			return Result.WIN;
+		}
+
+		return Result.TIE;
+	}
+
+
+
+	private String[] cardStringToArray(String hand){
+		return hand.split(" ");
+	}
+
+	private Card[] extractHighestCardsFromMap(HashMap<String, Integer> cards){
+		Card[] highestCards = new Card[2];
+		int count = 0;
+		for (Map.Entry<String, Integer> stringIntegerEntry : cards.entrySet()) {
+			if (stringIntegerEntry.getValue() == 2 ) {
+				highestCards[count] = Card.stringToCard(valueOf(stringIntegerEntry.getKey().charAt(0)));
+				count++;
+			}
+		}
+		return highestCards;
+	}
+
+	private Card extractHighestCardFromMap(HashMap<String, Integer> cards, int occurrence){
+		Card highestCard = null;
+		for (Map.Entry<String, Integer> stringIntegerEntry : cards.entrySet()) {
+			if (stringIntegerEntry.getValue() == occurrence ) {
+				highestCard = Card.stringToCard(valueOf(stringIntegerEntry.getKey().charAt(0)));
+			}
+		}
+		return highestCard;
+	}
+
+	private Result compareHighCard(PokerHand opponentsHand) {
 
 			if (getHighestCard().value() < opponentsHand.getHighestCard().value()) {
 				return Result.LOSS;
@@ -47,7 +129,7 @@ public class PokerHand {
 	}
 
 	public Card getHighestCard() {
-		String[] splitHand = hand.split(" ");
+		String[] splitHand = cardStringToArray(getHand());
 		Card highestCard = Card.stringToCard(valueOf(splitHand[0].charAt(0)));
 
 		for (int i = 1; i < splitHand.length; i++) {
@@ -67,5 +149,19 @@ public class PokerHand {
 	public Rank getRank(){
 
 		return rankFactory.getRank(hand);
+	}
+
+	private HashMap<String, Integer> getStringIntegerHashMap(String[] splitHand) {
+		HashMap<String, Integer> cards = new HashMap<>();
+
+		for (String s : splitHand) {
+			if(!cards.containsKey(valueOf(s.charAt(0)))){
+				cards.put(valueOf(s.charAt(0)),1);
+			} else {
+				Integer value = cards.get(valueOf((s.charAt(0))));
+				cards.put(valueOf(s.charAt(0)),value+1);
+			}
+		}
+		return cards;
 	}
 }
--- src/test/java/com/poker/PokerHandTest.java
+++ src/test/java/com/poker/PokerHandTest.java
@@ -3,10 +3,8 @@ package com.poker;
 import com.poker.PokerHand.Result;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 /**
  * Unit test for simple App.
@@ -21,15 +19,15 @@ public class PokerHandTest {
 
 	@Test //(expected = IllegalArgumentException.class)
 	public void new_hand_test_null() {
-		new PokerHand(null, rankFactory);
+		new PokerHand(null);
 	}
 	
 	@Test
 	public void high_card_win() {
 		String cards1 = "As 2h 5c Jd Td";
 		String cards2 = "Kc 2s 5h Jh Tc";
-		PokerHand hand1 = new PokerHand(cards1, rankFactory);
-		PokerHand hand2 = new PokerHand(cards2, rankFactory);
+		PokerHand hand1 = new PokerHand(cards1);
+		PokerHand hand2 = new PokerHand(cards2);
 		assertEquals(Result.WIN, hand1.compareWith(hand2));
 		assertEquals(Result.LOSS, hand2.compareWith(hand1));
 	}
@@ -37,7 +35,7 @@ public class PokerHandTest {
 	@Test
 	public void get_highest_card_ace(){
 		String cards1 = "As 2h 5c Jd Td";
-		PokerHand hand1 = new PokerHand(cards1, rankFactory);
+		PokerHand hand1 = new PokerHand(cards1);
 		assertEquals(Card.ACE, hand1.getHighestCard());
 	}
 
@@ -45,8 +43,8 @@ public class PokerHandTest {
 	public void royal_flush_win() {
 		String royalFlush = "Th Jh Qh Kh Ah";
 		String straightFlush = "3s 4s 5s 6s 7s";
-		PokerHand hand1 = new PokerHand(royalFlush, rankFactory);
-		PokerHand hand2 = new PokerHand(straightFlush, rankFactory);
+		PokerHand hand1 = new PokerHand(royalFlush);
+		PokerHand hand2 = new PokerHand(straightFlush);
 		assertEquals(Result.WIN, hand1.compareWith(hand2));
 		assertEquals(Result.LOSS, hand2.compareWith(hand1));
 	}
@@ -55,8 +53,8 @@ public class PokerHandTest {
 	public void royal_flush_tie(){
 		String royalFlush = "Th Jh Qh Kh Ah";
 		String straightFlush = "Th Jh Qh Kh Ah";
-		PokerHand hand1 = new PokerHand(royalFlush, rankFactory);
-		PokerHand hand2 = new PokerHand(straightFlush, rankFactory);
+		PokerHand hand1 = new PokerHand(royalFlush);
+		PokerHand hand2 = new PokerHand(straightFlush);
 		assertEquals(Result.TIE, hand1.compareWith(hand2));
 		assertEquals(Result.TIE, hand2.compareWith(hand1));
 	}
@@ -65,38 +63,48 @@ public class PokerHandTest {
 	public void straight_flush_higher_win(){
 		String straightFlushLower  =  "3s 4s 5s 6s 7s";
 		String straightFlushHigher =  "4s 5s 6s 7s 8s";
-		PokerHand hand1 = new PokerHand(straightFlushLower, rankFactory);
-		PokerHand hand2 = new PokerHand(straightFlushHigher, rankFactory);
+		PokerHand hand1 = new PokerHand(straightFlushLower);
+		PokerHand hand2 = new PokerHand(straightFlushHigher);
 		assertEquals(Result.LOSS, hand1.compareWith(hand2));
 		assertEquals(Result.WIN, hand2.compareWith(hand1));
 	}
 
 	@Test
 	public void four_of_a_kind_higher_win(){
-		String fourOfAKindHigher  =  "10h 10s 10c 10d 7s";
+		String fourOfAKindHigher  =  "Th Ts Tc Td 7s";
 		String fourOfAKindLower =  "9h 9s 9c 9d 8s";
-		PokerHand hand1 = new PokerHand(fourOfAKindHigher, rankFactory);
-		PokerHand hand2 = new PokerHand(fourOfAKindLower, rankFactory);
+		PokerHand hand1 = new PokerHand(fourOfAKindHigher);
+		PokerHand hand2 = new PokerHand(fourOfAKindLower);
 		assertEquals(Result.WIN, hand1.compareWith(hand2));
 		assertEquals(Result.LOSS, hand2.compareWith(hand1));
 	}
 
 	@Test
 	public void four_of_a_kind_wins(){
-		String fourOfAKind  =  "10h 10s 10c 10d 7s";
+		String fourOfAKind  =  "Th Ts Tc Td 7s";
 		String threeOfAKind = "7h 7s 7c 9d 3s";
-		PokerHand hand1 = new PokerHand(fourOfAKind, rankFactory);
-		PokerHand hand2 = new PokerHand(threeOfAKind, rankFactory);
+		PokerHand hand1 = new PokerHand(fourOfAKind);
+		PokerHand hand2 = new PokerHand(threeOfAKind);
 		assertEquals(Result.WIN, hand1.compareWith(hand2));
 		assertEquals(Result.LOSS, hand2.compareWith(hand1));
 	}
 
 	@Test
 	public void three_of_a_kind_wins(){
-		String threeOfAKind  =  "Ah As Ac 10d 7s";
+		String threeOfAKind  =  "Ah As Ac Td 7s";
 		String onePair = "7h 7s 5c 9d 3s";
-		PokerHand hand1 = new PokerHand(threeOfAKind, rankFactory);
-		PokerHand hand2 = new PokerHand(onePair, rankFactory);
+		PokerHand hand1 = new PokerHand(threeOfAKind);
+		PokerHand hand2 = new PokerHand(onePair);
+		assertEquals(Result.WIN, hand1.compareWith(hand2));
+		assertEquals(Result.LOSS, hand2.compareWith(hand1));
+	}
+
+	@Test
+	public void three_of_a_kind_highest_cards_win(){
+		String threeOfAKindHigher  =  "Ah As Ac Td 7s";
+		String threeOfAKindLower = "3h 3s 3c Ad 7h";
+		PokerHand hand1 = new PokerHand(threeOfAKindHigher);
+		PokerHand hand2 = new PokerHand(threeOfAKindLower);
 		assertEquals(Result.WIN, hand1.compareWith(hand2));
 		assertEquals(Result.LOSS, hand2.compareWith(hand1));
 	}
@@ -105,18 +113,27 @@ public class PokerHandTest {
 	public void two_pairs_wins(){
 		String twoPairs = "7h 7s Ah As 3s";
 		String onePair  = "3h 3s 7d Ac Td";
-		PokerHand hand1 = new PokerHand(twoPairs, rankFactory);
-		PokerHand hand2 = new PokerHand(onePair, rankFactory);
+		PokerHand hand1 = new PokerHand(twoPairs);
+		PokerHand hand2 = new PokerHand(onePair);
 		assertEquals(Result.WIN, hand1.compareWith(hand2));
 		assertEquals(Result.LOSS, hand2.compareWith(hand1));
 	}
-	// flush and straight
+
+	@Test
+	public void two_pairs_highest_pair_wins(){
+		String twoPairsHigher = "Kh Ks Ah As 3s";
+		String twoPairLower  = "7h 7s 3d 3c Ad";
+		PokerHand hand1 = new PokerHand(twoPairsHigher);
+		PokerHand hand2 = new PokerHand(twoPairLower);
+		assertEquals(Result.WIN, hand1.compareWith(hand2));
+		assertEquals(Result.LOSS, hand2.compareWith(hand1));
+	}
 	@Test
 	public void flush_wins(){
 		String flush = "2h 6h 9h Qh Kh";
 		String straight = "3d 4h 5s 6c 7d";
-		PokerHand hand1 = new PokerHand(flush, rankFactory);
-		PokerHand hand2 = new PokerHand(straight, rankFactory);
+		PokerHand hand1 = new PokerHand(flush);
+		PokerHand hand2 = new PokerHand(straight);
 		assertEquals(Result.WIN, hand1.compareWith(hand2));
 		assertEquals(Result.LOSS, hand2.compareWith(hand1));
 	}
@@ -125,8 +142,8 @@ public class PokerHandTest {
 	public void higher_flush_wins(){
 		String higherFlush = "2h 6h 9h Qh Ah";
 		String lowerFlush = "2d 5d 6d 8d Kd";
-		PokerHand hand1 = new PokerHand(higherFlush, rankFactory);
-		PokerHand hand2 = new PokerHand(lowerFlush, rankFactory);
+		PokerHand hand1 = new PokerHand(higherFlush);
+		PokerHand hand2 = new PokerHand(lowerFlush);
 		assertEquals(Result.WIN, hand1.compareWith(hand2));
 		assertEquals(Result.LOSS, hand2.compareWith(hand1));
 	}
@@ -135,8 +152,8 @@ public class PokerHandTest {
 	public void straight_wins(){
 		String straight = "3d 4h 5s 6c 7d";
 		String highCard = "As 2h 5c Jd Td";
-		PokerHand hand1 = new PokerHand(straight, rankFactory);
-		PokerHand hand2 = new PokerHand(highCard, rankFactory);
+		PokerHand hand1 = new PokerHand(straight);
+		PokerHand hand2 = new PokerHand(highCard);
 		assertEquals(Result.WIN, hand1.compareWith(hand2));
 		assertEquals(Result.LOSS, hand2.compareWith(hand1));
 	}
